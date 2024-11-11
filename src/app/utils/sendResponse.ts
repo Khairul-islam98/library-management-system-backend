b41d0a7a9ac5305bc5479eb096d34f7567fc8bb3
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-11 18:13:47 +0600

    add utility functions for error handling and standardized response formatting

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,19 @@
+import { Response } from "express";
+
+type ResponseType<T> = {
+  statusCode: number;
+  success: boolean;
+  message?: string;
+  data: T;
+};
+
+const sendResponse = <T>(res: Response, data: ResponseType<T>) => {
+  res.status(data.statusCode).json({
+    success: data.success,
+    status: data.statusCode,
+    message: data.message,
+    data: data.data,
+  });
+};
+
+export default sendResponse;
